50e2148ec1051b5b8e02e1e4a5ce42cec9e41cda
RonanMc <ronanmcclelland@gmail.com>
2020-06-01 20:58:39 +0100

    Add signup form class

--- podcastbuzz/forms.py
+++ podcastbuzz/forms.py
@@ -1,8 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, EqualTo
 
 class LogonForm(FlaskForm):
+    '''Log on to the app'''
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log in')
+
+class SignupForm(FlaskForm):
+    '''Register for the app'''
+    username = StringField('Username', validators=[DataRequired(), Length(min=6, max=25)])
+    password = StringField('Password', validators=[DataRequired(), Length(min=6, max=25)])
+    confirm = PasswordField('Repeat Password',[DataRequired(),EqualTo('password', message='Passwords do not match')])
+    email = StringField('Email', validators=[DataRequired()])
