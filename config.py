c8bf77410e9796aabf1ae2feffab1544eae5ed8b
RonanMc <ronanmcclelland@gmail.com>
2020-06-18 15:16:54 +0100

    Removed extra whitespace

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 
 class Config(object):
-    
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-should-be-a-secret-string'
     MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
     MONGO_URI = os.environ.get('MONGO_URI')
