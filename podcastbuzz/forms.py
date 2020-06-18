8f3b86a73f2c4f90bbe9868cdf0576846a681d6e
RonanMc <ronanmcclelland@gmail.com>
2020-06-18 10:58:30 +0100

    Added email validation to form

--- podcastbuzz/forms.py
+++ podcastbuzz/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Email
+
 
 class LogonForm(FlaskForm):
     '''Log on to the app'''
@@ -8,10 +9,11 @@ class LogonForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log in')
 
+
 class SignupForm(FlaskForm):
     '''Register for the app'''
     username = StringField('Username', validators=[DataRequired(), Length(min=6, max=25)])
     password = StringField('Password', validators=[DataRequired(), Length(min=6, max=25)])
     confirm = PasswordField('Repeat Password',[DataRequired(),EqualTo('password', message='Passwords do not match')])
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Register')
