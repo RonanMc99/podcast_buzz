3607b60f9f45fbe4c0c684cac1c83f271f44a1c9
RonanMc <ronanmcclelland@gmail.com>
2020-06-18 15:07:02 +0100

    Updated app name to 'run'

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 
 class Config(object):
+    
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-should-be-a-secret-string'
     MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
     MONGO_URI = os.environ.get('MONGO_URI')
