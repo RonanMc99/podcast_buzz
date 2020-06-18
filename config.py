024c4300e8a7568bac926e5bb415fec5a5d219c8
RonanMc <ronanmcclelland@gmail.com>
2020-06-18 14:59:35 +0100

    Fixed issue with indentation

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-# set required configuration items as class variables
+
 class Config(object):
-        SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-should-be-a-secret-string'
-        MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
-        MONGO_URI = os.environ.get('MONGO_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-should-be-a-secret-string'
+    MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
+    MONGO_URI = os.environ.get('MONGO_URI')
