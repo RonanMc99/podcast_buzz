3beccc1ee6cdea8fac1e3a55fa7586395218ecc2
RonanMc <ronanmcclelland@gmail.com>
2020-06-19 15:31:04 +0100

    Create model to manage user sessions

---
+++ podcastbuzz/models.py
@@ -0,0 +1,19 @@
+from podcast_buzz import login_manager, mongo
+from bson.objectid import ObjectId
+
+
+# Use Flask_login to manage user sessions
+@login_manager.user_loader
+def load_user(user_id):
+    users = mongo.db.users
+    user_json = users.find_one({'_id': ObjectId(user_id)})
+    return User(user_json)
+
+
+# UserMixIn provides default implementations for the methods 
+# that Flask-Login expects user objects to have
+
+
+Class User(UserMixIn):
+    def __init__(self, user_json):
+        self.user_json = user_json
