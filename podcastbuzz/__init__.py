be9ba4b6c190748ea56c96e9e44e63918a9fc1b1
RonanMc <ronanmcclelland@gmail.com>
2020-06-19 15:08:09 +0100

    Create LoginManager instance

--- podcastbuzz/__init__.py
+++ podcastbuzz/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from config import Config
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
-
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'logon'
 
 # placing imports at the bottom to avoid circular dependancy issues
 from podcastbuzz import routes
--- podcastbuzz/routes.py
+++ podcastbuzz/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, redirect, url_for, request, flash
+from flask_login import current_user
 from podcastbuzz import app, mongo, bcrypt
 from podcastbuzz.forms import LogonForm, SignupForm
 
