780c60895b423a069582dc055eb7c80db189ef5e
RonanMc <ronanmcclelland@gmail.com>
2020-06-20 14:31:39 +0100

    Fixed incorrect variable name and pword field type

--- podcastbuzz/forms.py
+++ podcastbuzz/forms.py
@@ -14,7 +14,7 @@ class LogonForm(FlaskForm):
 class SignupForm(FlaskForm):
     '''Register for the app'''
     username = StringField('Username', validators=[DataRequired(), Length(min=6, max=25)])
-    password = StringField('Password', validators=[DataRequired(), Length(min=6, max=25)])
-    confirm = PasswordField('Repeat Password',[DataRequired(),EqualTo('password', message='Passwords do not match')])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=25)])
+    confirmpassword = PasswordField('Repeat Password',[DataRequired(), EqualTo('password', message='Passwords do not match')])
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Register')
