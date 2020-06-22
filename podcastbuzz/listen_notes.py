5e63b08a7e9c43cdc88bb851f938cbb9375f63cc
RonanMc <ronanmcclelland@gmail.com>
2020-06-22 15:14:50 +0100

    Added ListenNotes search API

---
+++ podcastbuzz/listen_notes.py
@@ -0,0 +1,17 @@
+import os
+import requests
+import json
+
+ListenAPI_Key = os.environ.get("X_LISTENAPI_KEY")
+
+
+def search_podcast(search_param):
+    url = "https://listen-api.listennotes.com/api/v2/search?q=" + str(search_param)
+
+    # payload = (('q', search_params),)
+    headers = {
+        'X-ListenAPI-Key': ListenAPI_Key
+    }
+    response = requests.request('GET', url, headers=headers)
+    response = json.loads(response.text)
+    return(response)
