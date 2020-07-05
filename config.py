7f3d1f265c8db278a87aac375c649c25d534dd43
RonanMc <ronanmcclelland@gmail.com>
2020-07-05 19:29:44 +0100

    Update MongoDB URI

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-should-be-a-secret-string'
     MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
-    MONGO_URI = os.environ.get('MONGO_URI')
+    MONGO_URI = os.environ.get('MONGODB_URI')
