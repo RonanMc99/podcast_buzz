bad60f38261a209fe96742d0961ef0d6d9d4cec2
RonanMc <ronanmcclelland@gmail.com>
2020-07-06 19:44:15 +0100

    Setting IP and port in the app config

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-should-be-a-secret-string'
     MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
     MONGO_URI = os.environ.get('MONGO_URI')
+    IP = os.environ.get('IP')
+    PORT = os.environ.get('PORT')
