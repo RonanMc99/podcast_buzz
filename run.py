eae967f5edffaa90292a4753da0909557cc119e3
RonanMc <ronanmcclelland@gmail.com>
2020-07-06 18:23:14 +0100

    Updated to specify port 5000

--- run.py
+++ run.py
@@ -3,4 +3,6 @@ import os
 from podcastbuzz import app
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
+    # app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
