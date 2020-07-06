fdb4a33536d8a2e782a37e8ed553570380e55b23
RonanMc <ronanmcclelland@gmail.com>
2020-07-06 19:02:00 +0100

    Attempting to set port and IP only in config vars

--- run.py
+++ run.py
@@ -3,6 +3,4 @@ import os
 from podcastbuzz import app
 
 if __name__ == '__main__':
-    # app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
