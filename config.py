57204655c64c46b84399714c828272eba66876f5
RonanMc <ronanmcclelland@gmail.com>
2020-07-06 17:34:40 +0100

    Clean up config (mlab unused)

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-should-be-a-secret-string'
     MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
     MONGO_URI = os.environ.get('MONGO_URI')
-    MONGODB_URI = os.environ.get('MONGODB_URI')
