b796e824bb1eaf1f0d3f209b3ba2eb295443049a
RonanMc <ronanmcclelland@gmail.com>
2020-07-06 19:07:56 +0100

    Setting the IP & port

--- run.py
+++ run.py
@@ -2,5 +2,7 @@ import os
 
 from podcastbuzz import app
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=os.environ.get("PORT"),
+            debug=True)
