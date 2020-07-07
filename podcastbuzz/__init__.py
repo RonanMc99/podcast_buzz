d610812b5b6430ddfbc320adf28e807ae947a44a
RonanMc <ronanmcclelland@gmail.com>
2020-07-07 12:08:38 +0100

    Try setting app config items individually

--- podcastbuzz/__init__.py
+++ podcastbuzz/__init__.py
@@ -4,12 +4,13 @@ from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
-app.config.from_envar('SECRET_KEY')
-app.config.from_envar('MONGO_DBNAME')
-app.config.from_envar('MONGO_URI')
+app.config['SECRET_KEY'] = "os.environ.get('SECRET_KEY')"
+app.config['MONGO_DBNAME'] = "os.environ.get('MONGO_DBNAME')"
+app.config['MONGO_URI'] = "os.environ.get('MONGO_URI')"
 # app.config.from_object(Config)
 mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
