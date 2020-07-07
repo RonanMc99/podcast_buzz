2e1d52540dd56092c6bd5cdb2cc31723c60c34e9
RonanMc <ronanmcclelland@gmail.com>
2020-07-07 10:20:35 +0100

    Add IP and Port back (heroku)

--- run.py
+++ run.py
@@ -3,4 +3,6 @@ import os
 from podcastbuzz import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=os.environ.get('IP'),
+            port=os.environ.get('PORT'),
+            debug=True)
