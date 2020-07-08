29e9da7fbb4c47695e39870f284031c2bacf40ab
RonanMc <ronanmcclelland@gmail.com>
2020-07-08 08:53:25 +0100

    Remove SERVER_NAME

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-should-be-a-secret-string'
     MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
     MONGO_URI = os.environ.get('MONGO_URI')
-    SERVER_NAME = '0.0.0.0:5000'
+    # SERVER_NAME = '0.0.0.0:5000'
